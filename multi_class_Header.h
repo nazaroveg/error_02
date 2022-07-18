9c40fb98e74359b339f2109ef3504b1574889bf5
nazaroveg <nazaroveg@yandex.ru>
2022-07-18 15:07:58 +0300

    v6.

--- multi_class_Header.h
+++ multi_class_Header.h
@@ -3,19 +3,7 @@
 
 
 
-#include "multi_class_Firure.h"
-#include "multi_class_triangle.h"
-//#include "multi_class_Equilateral_triangle.h"
-//#include "multi_class_Isosceles_triangle.h"
-//#include "multi_class_Parallelogram.h"
-//#include "multi_class_Quadrilateral.h"
-//#include "multi_class_Rectangle.h"
-//#include "multi_class_Rhombus.h"
-//#include "multi_class_Right_triangle.h"
-//#include "multi_class_Square.h"
 
-
-class Figure{};
 
 
 
